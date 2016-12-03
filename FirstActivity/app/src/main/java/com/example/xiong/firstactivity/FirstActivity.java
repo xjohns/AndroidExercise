693acd73657cacbb58ececd2bdda2aa8b17aaa4f
xjohns <2867193240@qq.com>
2016-12-03 23:08:32 +0800

    Intent的练习

---
+++ FirstActivity/app/src/main/java/com/example/xiong/firstactivity/FirstActivity.java
@@ -0,0 +1,62 @@
+package com.example.xiong.firstactivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //显式Intent
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                startActivity(intent);
+                //隐式Intent
+//                Intent intent = new Intent("com.example.xiong.firstactivity.ACTION_START");
+//                startActivity(intent);
+                //隐式Intent调用系统浏览器
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+                //隐式Intent调用系统拨号
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10010"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add:{
+                Toast.makeText(this,"add",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.remove:{
+                Toast.makeText(this,"remove",Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return true;
+    }
+}
---
+++ FirstActivity/app/src/main/java/com/example/xiong/firstactivity/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.xiong.firstactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xiong on 2016/12/3.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
