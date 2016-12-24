f4ba205f95ff8a461bf735fecef4342f8e11f735
xjohns <2867193240@qq.com>
2016-12-24 23:14:25 +0800

    SQLite数据库练习
    Signed-off-by: xjohns <2867193240@qq.com>

---
+++ DatabaseTest/app/src/main/java/com/example/xiong/databasetest/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.xiong.databasetest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button btn_createDb = (Button) findViewById(R.id.btn_createDatabase);
+        btn_createDb.setOnClickListener(this);
+        Button btn_addData = (Button) findViewById(R.id.btn_addData);
+        btn_addData.setOnClickListener(this);
+        Button btn_updateData = (Button) findViewById(R.id.btn_updateData);
+        btn_updateData.setOnClickListener(this);
+        Button btn_deleteData = (Button) findViewById(R.id.btn_deleteData);
+        btn_deleteData.setOnClickListener(this);
+        Button btn_queryData = (Button) findViewById(R.id.btn_queryData);
+        btn_queryData.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_createDatabase:{
+                /*
+                 getReadableDatabase()和getWritableDatabase()。这两个方法都可以创建或打开一个现有的数据库（如果数据库已存在则直接打开，否则创建一个新的数据库），
+                 并返回一个可对数据库进行读写操作的对象，当数据库不可写入的时候（如磁盘空间已满）getReadableDatabase()方法返回的对象将以只读的方式去打开数据库，
+                 而 getWritableDatabase()方法则将出现异常。
+                 */
+                dbHelper.getWritableDatabase();
+                break;
+            }
+            case R.id.btn_addData:{
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues contentValues = new ContentValues();
+                //开始组装第一条数据
+                contentValues.put("name", "The Da Vinci Code");
+                contentValues.put("author", "Dan Brown");
+                contentValues.put("pages", 454);
+                contentValues.put("price", 16.96);
+                db.insert("Book", null, contentValues);//插入第一条数据
+                contentValues.clear();
+                contentValues.put("name", "The Lost Symbol");
+                contentValues.put("author", "Dan Brown");
+                contentValues.put("pages", 510);
+                contentValues.put("price", 19.95);
+                db.insert("Book", null, contentValues);//插入第二条数据
+                break;
+            }
+            case R.id.btn_updateData:{
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("price", 10.96);
+                /*
+                更新数据，第三、第四个参数用于去约束更新某一行或某几行中的数据，不指定的话默认就是更新所有行，
+                ?是一个占位符，可以通过第四个参数提供的一个字符串数组为第三个参数中的每个占位符指定相应的内容。
+                 */
+                db.update("Book", contentValues, "name = ?", new String[]{"The DaVinci Code"});
+                break;
+            }
+            case R.id.btn_deleteData:{
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //第二、第三个参数又是用于去约束删除某一行或某几行的数据，不指定的话默认就是删除所有行。
+                db.delete("Book", "pages > ?", new String[]{"500"});
+                break;
+            }
+            case R.id.btn_queryData:{
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                /*
+                query()第二个参数用于指定去查询哪几列，如果不指定则默认查询所有列。第三、第四个参数用于去约束查询某一行或某几行的数据，
+                不指定则默认是查询所有行的数据。第五个参数用于指定需要去 group by 的列，不指定则表示不对查询结果进行 group by 操作。
+                第六个参数用于对 group by 之后的数据进行进一步的过滤，不指定则表示不进行过滤。第七个参数用于指定查询结果的排序方式，
+                不指定则表示使用默认的排序方式。
+                 */
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()){
+                    do {
+                        /*
+                        遍历Cursor对象，取出数据并打印，通过Cursor的getColumnIndex()方法获取到某一列在表中对应的位置索引，
+                        然后将这个索引传入到相应的取值方法中，可得到从数据库中读取到的数据。
+                         */
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+                break;
+            }
+        }
+    }
+}
---
+++ DatabaseTest/app/src/main/java/com/example/xiong/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.xiong.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by xiong on 2016/12/21.
+ */
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    //Book表的SQL建表语句
+    public static final String CREATE_BOOK = "create table Book (" +
+            "id integer primary key autoincrement," +
+            "author text," +
+            "price real," +
+            "pages integer," +
+            "name text)";
+    public static final String CREATE_CATEGORY = "create table Category(" +
+            "id integer primary key autoincrement," +
+            "categoty_name text," +
+            "category_code integer)";
+    private Context mContext;
+    //第三个参数允许我们在查询数据的时候返回一个自定义的Cursor，一般都是传入 null，第四个参数表示当前数据库的版本号
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);//调用SQLiteDatabase的execSQL()方法去执行SQL语句
+        Toast.makeText(mContext, "创建成功", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+    }
+}
