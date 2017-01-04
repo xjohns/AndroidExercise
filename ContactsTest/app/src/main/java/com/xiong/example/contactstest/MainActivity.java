56fdfaa061b05be08661c109ab8629d74d23f47a
xjohns <2867193240@qq.com>
2017-01-04 13:52:49 +0800

    通过ContentProvider获取联系人信息

---
+++ ContactsTest/app/src/main/java/com/xiong/example/contactstest/MainActivity.java
@@ -0,0 +1,48 @@
+package com.xiong.example.contactstest;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView contactsView;
+    ArrayAdapter<String> adapter;
+    List<String> contactsList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contactsView = (ListView) findViewById(R.id.tView_contacts);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsList);
+        contactsView.setAdapter(adapter);
+        readContacts();
+    }
+
+    private void readContacts() {
+        Cursor cursor = null;
+        try {
+            //查询联系人数据
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while (cursor.moveToNext()){
+                //获取联系人姓名
+                String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                //获取联系人手机号
+                String numble = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contactsList.add(displayName + "\n" +numble);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (cursor != null){
+                cursor.close();
+            }
+        }
+    }
+}
